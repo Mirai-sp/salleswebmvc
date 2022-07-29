c882e2955853e13ba01c973f782880e3c1838b0e
Denis Queiroz <denisxx07@hotmail.com>
2022-07-29 19:36:39 -0300

    Classes, Enums...

---
+++ SallesWebMvc/Models/Enums/SalesStatus.cs
@@ -0,0 +1,9 @@
+﻿namespace SallesWebMvc.Models.Enums
+{
+    public enum SalesStatus : int
+    {
+        Pending = 0,
+        Billed = 1,
+        Canceled = 2
+    }
+}
--- SallesWebMvc/Startup.cs
+++ SallesWebMvc/Startup.cs
@@ -37,7 +37,8 @@ namespace SallesWebMvc
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             services.AddDbContext<SallesWebMvcContext>(options =>
-                    options.UseSqlServer(Configuration.GetConnectionString("SallesWebMvcContext")));
+                    options.UseMySql(Configuration.GetConnectionString("SallesWebMvcContext"), builder =>
+                    builder.MigrationsAssembly("SallesWebMvc")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
