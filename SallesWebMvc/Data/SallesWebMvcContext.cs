94ce60a66b2fdf4f17f641af0dba17974f0e8348
Denis Queiroz <denisxx07@hotmail.com>
2022-07-29 14:31:00 -0300

    Crud Scaffolding

---
+++ SallesWebMvc/Data/SallesWebMvcContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SallesWebMvc.Models;
+
+namespace SallesWebMvc.Data
+{
+    public class SallesWebMvcContext : DbContext
+    {
+        public SallesWebMvcContext (DbContextOptions<SallesWebMvcContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SallesWebMvc.Models.Department> Department { get; set; }
+    }
+}
--- SallesWebMvc/Startup.cs
+++ SallesWebMvc/Startup.cs
@@ -9,6 +9,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SallesWebMvc.Data;
 
 namespace SallesWebMvc
 {
@@ -33,6 +35,9 @@ namespace SallesWebMvc
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<SallesWebMvcContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("SallesWebMvcContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
